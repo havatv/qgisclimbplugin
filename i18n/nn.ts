62c5626f92ee7805bee4ade362d44d6ed08ee752
havatv <havard.tveite@nmbu.no>
2019-03-12 12:33:14 +0100

    included translation files

---
+++ i18n/nn.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>ClimbAlgorithm</name>
+    <message>
+        <location filename="../Climb_algorithm.py" line="106"/>
+        <source>DEM (to get Z values)</source>
+        <translation>DEM (for å hente Z-verdier)</translation>
+    </message>
+    <message>
+        <location filename="../Climb_algorithm.py" line="115"/>
+        <source>Band containing the Z values)</source>
+        <translation>Bånd med Z-verdier)</translation>
+    </message>
+    <message>
+        <location filename="../Climb_algorithm.py" line="287"/>
+        <source>Climb along line</source>
+        <translation>Stigning langs linje</translation>
+    </message>
+    <message>
+        <location filename="../Climb_algorithm.py" line="294"/>
+        <source>Vector analysis</source>
+        <translation>Vektor-analyse</translation>
+    </message>
+    <message>
+        <location filename="../Climb_algorithm.py" line="97"/>
+        <source>Input (line) layer</source>
+        <translation>Innlag (linjer)</translation>
+    </message>
+    <message>
+        <location filename="../Climb_algorithm.py" line="125"/>
+        <source>Climb layer</source>
+        <translation>Stigningslag</translation>
+    </message>
+</context>
+<context>
+    <name>ClimbProvider</name>
+    <message>
+        <location filename="../Climb_provider.py" line="74"/>
+        <source>Climb</source>
+        <translation>Stigning</translation>
+    </message>
+</context>
+</TS>
